7340187717801b34e3c9a28f48cca9e51e17fbc0
vaibhu6 <vaibhavisawant75@gmail.com>
2018-11-26 18:38:49 +0530

    Files added

---
+++ Collection Framewrork/src/com/vaibhu/cf/Question4.java
@@ -0,0 +1,16 @@
+package com.vaibhu.cf;
+
+import java.util.HashMap;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		
+		HashMap<Integer,String> hash1= new HashMap<Integer,String>();
+		hash1.put( 06/06/1996 , "Vaibhavi" );
+		hash1.put( 06/06/1996 , "Vinit" );
+		hash1.put( 06/06/1996 , "Kajal" );
+	
+		
+}
+}
---
+++ Sample/src/TreeSetDemo.java
@@ -0,0 +1,23 @@
+
+	import java.io.*; 
+	import java.util.TreeSet; 
+	  
+	public class TreeSetDemo { 
+	    public static void main(String args[]) 
+	    { 
+	        // Creating an empty TreeSet 
+	        TreeSet<String> tree = new TreeSet<String>(); 
+	  
+	        // Use add() method to add elements into the Set 
+	        tree.add("Welcome"); 
+	        tree.add("To"); 
+	        tree.add("Geeks"); 
+	        tree.add("4"); 
+	        tree.add("Geeks"); 
+	        tree.add("TreeSet"); 
+	  
+	        // Displaying the TreeSet 
+	        System.out.println("TreeSet: " + tree); 
+	    } 
+	} 
+
